ca2da68efb63dc682f175fa20923aa232deb381e
Jacek Schae <jacek.schae@gmail.com>
2017-09-06 08:22:06 +0200

    Add to order

--- src/cljs/reagent_for_beginners/components/fish.cljs
+++ src/cljs/reagent_for_beginners/components/fish.cljs
@@ -2,10 +2,9 @@
   (:require [reagent-for-beginners.state :as state]))
 
 (defn add-to-order [orders order]
-  (let [m {:fish-id amount}])
-  (.log js/console orders)
-  (.log js/console (contains? m (keyword order)))
-  (swap! orders conj (keyword order) 1))
+  (if (contains? @orders (keyword order))
+    (swap! orders conj (update-in @orders [(keyword order)] inc))
+    (swap! orders conj {(keyword order) 1})))
 
 (defn component [id name price status image desc]
   [:li.menu-fish
