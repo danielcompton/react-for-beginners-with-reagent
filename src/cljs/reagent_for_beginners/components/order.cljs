e4b36a5070762a30b01ca54bb73d5d3a06902afe
Jacek Schae <jacek.schae@gmail.com>
2017-09-21 21:00:30 +0200

    Render order at Your Order

--- src/cljs/reagent_for_beginners/components/order.cljs
+++ src/cljs/reagent_for_beginners/components/order.cljs
@@ -1,5 +1,16 @@
-(ns reagent-for-beginners.components.order)
+(ns reagent-for-beginners.components.order
+  (:require [reagent-for-beginners.state :as state]))
+
+(defn order [[id quantity]]
+  [:li
+    [:span quantity "order" id]
+    [:span.price quantity]])
 
 (defn component []
-  [:div
-   [:h2 "Order"]])
+  [:div.order-wrap
+   [:h2 "Your Order"]
+   [:ul.order
+    (for [{:keys [id quantity]} @state/orders]
+      ^{:key id} [order id quantity])]
+    
+   [:strong "Total:"]])
