22b1eec53cd7b0220b8b917f3e89db2168728c05
Jacek Schae <jacek.schae@gmail.com>
2017-10-07 18:49:45 +0200

    Filter for available fishes

--- src/cljs/reagent_for_beginners/components/order.cljs
+++ src/cljs/reagent_for_beginners/components/order.cljs
@@ -2,8 +2,6 @@
   (:require [reagent-for-beginners.state :as state]
             [reagent-for-beginners.helpers :as h]))
 
-
-
 (defn order [id quant]
   [:li
     [:span id]
@@ -11,13 +9,13 @@
 
 (defn total []
   (h/format-price (reduce (fn [prev key]
-            (let [fish @state/id-fishes
-                  count (key @state/orders)
-                  is-available (fish @state/id-fishes)]
-              (if true
-                (+ prev (* count (get (key @state/id-fishes) :price 1)))
-              prev)))
-          0 (keys @state/orders))))
+                            (let [fish @state/id-fishes
+                                  count (key @state/orders)
+                                  is-available (fish @state/id-fishes)]
+                              (if (= (get (key @state/id-fishes) :status) "available")
+                                (+ prev (* count (get (key @state/id-fishes) :price 1)))
+                                prev)))
+                          0 (keys @state/orders))))
 
 (defn component []
   [:div.order-wrap
@@ -27,5 +25,4 @@
       ^{:key id} [order id quant])
     [:li.total
      [:strong "Total:" (total)]]]])
-
 
