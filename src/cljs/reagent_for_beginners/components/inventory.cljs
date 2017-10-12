a15da078bb846d5b68da51eeb14264b6562403de
Jacek Schae <jacek.schae@gmail.com>
2017-10-12 08:42:20 +0200

    Add sample fishes, cleanup state

--- src/cljs/reagent_for_beginners/components/inventory.cljs
+++ src/cljs/reagent_for_beginners/components/inventory.cljs
@@ -1,7 +1,11 @@
 (ns reagent-for-beginners.components.inventory
   (:require [reagent-for-beginners.components.add-fish-form :as add-fish-form]))
 
+(defn load-samples []
+  (.log js/console "Load Samples"))
+
 (defn component []
   [:div
    [:h2 "Inventory"]
-   [add-fish-form/component]])
+   [add-fish-form/component]
+   [:button {:on-click #(load-samples)} "Load Sample Fishes"]])
